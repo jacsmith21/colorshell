ac15b154548327757eaa3a8de4ae30ee787516b0
Jacob <jacob.smith@unb.ca>
2017-07-10 12:20:51 -0300

    added meta files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='colorshell',
+    packages = ['colorshell'],
+    version='0.1',
+    description='A simple package to abstract printing to IDLE in color',
+    author='Jacob Smith',
+    author_email='jacob.smith@unb.ca',
+    url='http://github.com/jacsmith21/colorshell',
+    download_url = 'http://github.com/jacsmith21/colorshell/dist/colorshell-0.1.tar.gz',
+    keywords = ['print', 'shell', 'IDLE', 'color', 'colour'],
+    classifiers = [],
+)
